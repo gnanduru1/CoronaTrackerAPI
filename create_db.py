5e89594ffcc56568e3776ce76500c39207c5dcab
gnanduru1 <ganesh.nanduru@gmail.com>
2020-08-03 17:44:51 -0400

    oops lol

--- create_db.py
+++ create_db.py
@@ -7,15 +7,11 @@ def parse(s):
     return {keys[i]:query.group(i+1) for i in range(len(keys))}
 
 def create_db():
-    # login = json.load('login.json')
-    login = parse("postgres://joe:password@host/dbname")
-    # login = parse(os.getenv('DATABASE_URL'))
+    with open('login.json', 'r') as f: login = json.load(f)
+    login = parse(os.getenv('DATABASE_URL'))
     login["port"] = 5432
-    with open("login.json", "w") as f:
-        json.dump(login, f)
 
-
-# conn = psycopg2.connect(database=login['database'], user=login['user'], password=login['password'], host=login['host'], port=login['port'])
+    conn = psycopg2.connect(database=login['database'], user=login['user'], password=login['password'], host=login['host'], port=login['port'])
 
 if __name__ == "__main__":
     create_db()
